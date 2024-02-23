f0a0354742a086a0e03b820af31bf20054da7dd6
Your Name <you@example.com>
2024-02-23 19:25:07 +0200

    task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,15 @@
-# Install an especific version of flask (2.1.0)
+# Install an especific version of werkzeug, flask (2.1.0)
+#!/usr/bin/pup
 package { 'werkzeug':
   ensure => '2.1.1',
   provider => 'pip3'
 }
 
-package { 'flask':
-  ensure => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
