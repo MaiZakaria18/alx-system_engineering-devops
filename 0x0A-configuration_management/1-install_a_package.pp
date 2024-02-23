f9adf44fa0c7d2e1928da237e1de6625d33d4005
Your Name <you@example.com>
2024-02-23 19:32:06 +0200

    task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
 # Install an especific version of werkzeug, flask (2.1.0)
 package { 'werkzeug':
-  ensure => '2.1.1',
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
 
 package { 'flask':
-  ensure => '2.1.0',
+  ensure   => '2.1.0',
   provider => 'pip3'
 }
