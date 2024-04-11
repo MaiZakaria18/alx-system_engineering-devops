aa2a724d26522e9480a3270d31a565cc6562ec92
Your Name <you@example.com>
2024-04-11 11:58:23 +0200

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""How many subs?"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    """
+    get number of subscribers
+    args:
+        subreddit: the name of subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url=url)
+    if (response.status_code != 200):
         return 0
+    subscribers = response.json()["data"]["subscribers"]
+    return (subscribers)
 
     return sub_info.json().get("data").get("subscribers")
